724f0865dcd6d46f0adc385a7d54b369a7d79ae0
Bernie Pope <bjpope@unimelb.edu.au>
2019-07-03 14:47:13 +1000

    Turn into proper shell script

--- .travis/unit-test.sh
+++ .travis/unit-test.sh
@@ -1,1 +1,17 @@
-mocha test/test.js
+#!/bin/bash
+
+set -e
+errors=0
+
+# Run unit tests
+mocha test/test.js || {
+    echo "'mocha test/test.js' failed"
+    let errors+=1
+}
+
+[ "$errors" -gt 0 ] && {
+    echo "There were $errors errors found"
+    exit 1
+}
+
+echo "Ok : JS specific tests"
